7420fc488aa0be66cd2325a3704885c21ddeb606
jzpoe <pmguez1287@gmail.com>
2025-06-14 21:01:11 -0500

    se agrega logo para la pestaña del navegador

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,11 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>ArtCreaciones</title>
+        <meta name="description" content="Descripción de tu página de trabajos escolares" />
+        <link rel="icon" href="/imgbody/logo.png" />
+      </head>
       <body>
         {children}
       </body>
